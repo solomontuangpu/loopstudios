bff0a6843944a7ddf6bbcb08141b4edcce1c03a6
solomontuangpu <solomontuangpu@gmail.com>
2024-04-03 20:09:12 -0400

    fix image not showing on netlify

--- src/imageData.ts
+++ src/imageData.ts
@@ -1,4 +1,21 @@
-import DeepEarth from "./assets/images/desktop/image-deep-earth.jpg";
+import DeepEarth from "./assets/images/desktop/image-deep-earth.jpg"
+import NightArcade from "./assets/images/desktop/image-night-arcade.jpg";
+import SoccerTeam from "./assets/images/desktop/image-soccer-team.jpg";
+import Grid from "./assets/images/desktop/image-grid.jpg";
+import FromAbove from "./assets/images/desktop/image-from-above.jpg";
+import Pocket from "./assets/images/desktop/image-pocket-borealis.jpg";
+import Curiosity from "./assets/images/desktop/image-curiosity.jpg";
+import FishEye from "./assets/images/desktop/image-fisheye.jpg";
+
+import DeepEarthMobile from "./assets/images/mobile/image-deep-earth.jpg";
+import NightArcadeMobile from "./assets/images/mobile/image-night-arcade.jpg";
+import SoccerTeamMobile from "./assets/images/mobile/image-soccer-team.jpg";
+import GridMobile from "./assets/images/mobile/image-grid.jpg";
+import FromAboveMobile from "./assets/images/mobile/image-from-above.jpg";
+import PocketMobile from "./assets/images/mobile/image-pocket-borealis.jpg";
+import CuriosityMobile from "./assets/images/mobile/image-curiosity.jpg";
+import FishEyeMobile from "./assets/images/mobile/image-fisheye.jpg";
+
 
 
 export const DESKTOP_IMG = [
@@ -10,44 +27,43 @@ export const DESKTOP_IMG = [
   },
   {
     id: 2,
-    imgPath: "/src/assets/images/desktop/image-night-arcade.jpg",
+    imgPath: NightArcade,
     titleTop: "night",
     titleBot: "arcade",
   },
   {
     id: 3,
-    imgPath: "/src/assets/images/desktop/image-soccer-team.jpg",
+    imgPath: SoccerTeam,
     titleTop: "soccer",
     titleBot: "team vr",
   },
   {
     id: 4,
-    imgPath: "/src/assets/images/desktop/image-grid.jpg",
+    imgPath: Grid,
     titleTop: "the",
     titleBot: "grid",
   },
   {
     id: 5,
-    imgPath: "/src/assets/images/desktop/image-from-above.jpg",
+    imgPath: FromAbove,
     titleTop: "from up",
     titleBot: "above vr",
   },
   {
     id: 6,
-    imgPath: "/src/assets/images/desktop/image-pocket-borealis.jpg"
-,
+    imgPath: Pocket,
     titleTop: "pocket",
     titleBot: "borealis",
   },
   {
     id: 7,
-    imgPath: "/src/assets/images/desktop/image-curiosity.jpg",
+    imgPath: Curiosity,
     titleTop: "the",
     titleBot: "curiosity",
   },
   {
     id: 8,
-    imgPath: "/src/assets/images/desktop/image-fisheye.jpg",
+    imgPath: FishEye,
     titleTop: "make it",
     titleBot: "fisheye",
   },
@@ -56,49 +72,49 @@ export const DESKTOP_IMG = [
 export const MOBILE_IMG = [
   {
     id: 1,
-    imgPath: "/src/assets/images/mobile/image-deep-earth.jpg",
+    imgPath: DeepEarthMobile,
     titleTop: "deep",
     titleBot: "earth",
   },
   {
     id: 2,
-    imgPath: "/src/assets/images/mobile/image-night-arcade.jpg",
+    imgPath:NightArcadeMobile,
     titleTop: "night",
     titleBot: "arcade",
   },
   {
     id: 3,
-    imgPath: "/src/assets/images/mobile/image-soccer-team.jpg",
+    imgPath: SoccerTeamMobile,
     titleTop: "soccer",
     titleBot: "team vr",
   },
   {
     id: 4,
-    imgPath: "/src/assets/images/mobile/image-grid.jpg",
+    imgPath: GridMobile,
     titleTop: "the",
     titleBot: "grid",
   },
   {
     id: 5,
-    imgPath: "/src/assets/images/mobile/image-from-above.jpg",
+    imgPath: FromAboveMobile,
     titleTop: "from up",
     titleBot: "above vr",
   },
   {
     id: 6,
-    imgPath: "/src/assets/images/mobile/image-pocket-borealis.jpg",
+    imgPath: PocketMobile,
     titleTop: "pocket",
     titleBot: "borealis",
   },
   {
     id: 7,
-    imgPath: "/src/assets/images/mobile/image-curiosity.jpg",
+    imgPath: CuriosityMobile,
     titleTop: "the",
     titleBot: "curiosity",
   },
   {
     id: 8,
-    imgPath: "/src/assets/images/mobile/image-fisheye.jpg",
+    imgPath: FishEyeMobile,
     titleTop: "make it",
     titleBot: "fisheye",
   },
